33d2cca25765d2c5c74bb8ac91a4a623a855d693
YusufSiddiquiWork <57582756+YusufSiddiquiWorks@users.noreply.github.com>
2025-01-17 22:13:08 +0530

    Update

--- application.py
+++ application.py
@@ -2,8 +2,6 @@ from flask import Flask, request, render_template
 import pickle
 import numpy as np
 import pandas as pd
-from xgboost import XGBRegressor
-from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
 app = application
